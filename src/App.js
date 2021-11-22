3d0459a61c6a5118877b9d9a3aeb4dd37b8aa3a6
Randy Wuchi <nnadoziennawuchi@yahoo.com>
2021-11-22 21:52:41 +0200

    created and styled the header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import { Header } from './components';
 import { Home, Shop } from './pages';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as MenuItem } from './menuItem/MenuItem';
 export { default as Directory } from './directory/Directory';
 export { default as PreviewCollection } from './previewCollection/PreviewCollection';
 export { default as ItemCollection } from './itemCollection/ItemCollection';
+export { default as Header } from './Header/Header';
